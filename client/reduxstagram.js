4e8e091044f21175f4678f565f9202aff52dacb9
Alex Griffis <alexgriffis@gmail.com>
2017-03-23 11:51:14 -0700

    Reset class

---
+++ client/reduxstagram.js
@@ -0,0 +1,7 @@
+// let's go!
+import React from 'react'
+import { render } from 'react-dom'
+// import css
+import css from './styles/style.styl'
+
+render(<h1>hi</h1>, document.getElementById('root'))
